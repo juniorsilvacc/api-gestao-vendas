3126fe2acd0ec36ade23bf35fc806b96c306d98d
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-09 13:36:32 -0300

    creation of product listing service

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,15 @@
+import { getCustomRepository } from 'typeorm';
+import { Product } from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+class ListProductService {
+  async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const products = await productsRepository.find();
+
+    return products;
+  }
+}
+
+export { ListProductService };
