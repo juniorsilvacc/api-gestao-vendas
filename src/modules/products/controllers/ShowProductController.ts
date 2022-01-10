a7206ab49b26da587a40698f9f7f5553f3078253
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-10 15:18:51 -0300

    controller and route creation to display a single product

---
+++ src/modules/products/controllers/ShowProductController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { ShowProductService } from '../services/ShowProductService';
+
+class ShowProductController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showProductService = container.resolve(ShowProductService);
+
+    const produtcs = await showProductService.execute({ id });
+
+    return response.json(produtcs);
+  }
+}
+
+export { ShowProductController };
--- src/shared/http/routes/products.routes.ts
+++ src/shared/http/routes/products.routes.ts
@@ -1,16 +1,19 @@
 import { CreateProductController } from '@modules/products/controllers/CreateProductController';
 import { DeleteProductController } from '@modules/products/controllers/DeleteProductController';
 import { ListProductController } from '@modules/products/controllers/ListProductController';
+import { ShowProductController } from '@modules/products/controllers/ShowProductController';
 import { Router } from 'express';
 
 const productsRoutes = Router();
 
 const createProductController = new CreateProductController();
 const listProductController = new ListProductController();
+const showProductController = new ShowProductController();
 const deleteProductController = new DeleteProductController();
 
 productsRoutes.post('/', createProductController.handle);
 productsRoutes.get('/', listProductController.handle);
+productsRoutes.get('/:id', showProductController.handle);
 productsRoutes.delete('/:id', deleteProductController.handle);
 
 export { productsRoutes };
