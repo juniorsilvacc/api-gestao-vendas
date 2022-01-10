bd4488d60b5567092479c85421e79524a4df3e6b
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-01-10 18:31:14 -0300

    creation of user migration and entity

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };
